fb6c7ea1ed73f95d2c9ad9a5e3a8e04cd014c7b7
Barotbek Turxonov <109985351+BarotbekTurxonov@users.noreply.github.com>
2023-02-12 23:53:01 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,7 +4,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN ="5971550033:AAFFx3giR8ipzJHr5Ji5vVcN8K2vEzvnW6k"     # Забираем значение типа str
+BOT_TOKEN ="BOT TOKEN HERE"     # Забираем значение типа str
 ADMINS =   [5235865310, 1635920175]    
                                      # Тут у нас будет список из админов
 CHANNEL = [-1001710909528]
